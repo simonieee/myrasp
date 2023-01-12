cbd3e550596732d14cca5d8459c4b96412f09510
simonieee <75366051+simonieee@users.noreply.github.com>
2023-01-12 21:42:05 +0900

    [UPDATE] ..

--- src/Layout/MainLayout/Components/Header.js
+++ src/Layout/MainLayout/Components/Header.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "../MainLayout.css";
+import Button from "@mui/material/Button";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return <div className="mainlayout-header">Blockchain Study</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="mainlayout-header">
+      <div className="wrap-header">
+        <nav>
+          <Button
+            id="home"
+            style={useLocation().pathname === "/" ? {} : { color: "#acacac" }}
+            onClick={() => navigate("/")}
+          >
+            Home
+          </Button>
+          <Button
+            id="pow"
+            style={
+              useLocation().pathname === "/pow" ? {} : { color: "#acacac" }
+            }
+            onClick={() => navigate("/pow")}
+          >
+            PoW_Simulator
+          </Button>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
